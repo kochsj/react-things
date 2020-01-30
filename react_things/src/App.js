f8740b84061b817d6803912560daa86064250215
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-01-29 20:28:44 -0800

    readme. ThingsList has state. Added event listener in Things list to 'clear' the value in the input after sending the value in the props.DoneHandler to App.js

--- react_things/src/App.js
+++ react_things/src/App.js
@@ -9,19 +9,50 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       thingList : [
-        {name:'Something'},
-        {name:'Other Thing'},
-        {name:'yet another thing'},
+        // {name:'Something', id:123},
+        // {name:'Other Thing', id:567},
+        // {name:'yet another thing', id:890},
       ]
     }
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+
+  // handleInputChange(event) {
+  //   const target = event.target;
+  //   const value = target.type === 'checkbox' ? target.checked : target.value;
+  //   const name = target.name;
+
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // }
+
   
+  handleFormSubmit(event){
+    event.preventDefault();
+
+    const target = event.target;
+    const value = target.itemToAdd.value
+
+    let key = (Math.random())*10_000
+    console.log('key: ', key)
+    let obj = {name: value, id: key}
+
+    let lst = this.state.thingList
+    lst.push(obj)
+    console.log('lst: ', lst)
+
+    this.setState({
+      thingList : lst
+    })
   }
 
   render(){
     return (
       <div className="App">
         <Header count={this.state.thingList.length}/>
-        <ThingList />
+        <ThingList list={this.state.thingList} doneHandler={this.handleFormSubmit}/>
         <Footer />
       </div>
     );
--- react_things/src/thingList.js
+++ react_things/src/thingList.js
@@ -1,5 +1,57 @@
 import React from 'react';
 
-export default function ThingList(props){
-    return <p>hello</p>
+export default class ThingList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            itemToAdd : ''
+        }
+        this.lst = props.list;
+        this.doneHandler = props.doneHandler
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.sendDoneHandler = this.sendDoneHandler.bind(this);
+
+    }
+    
+    handleInputChange(event) {
+        const value = event.target.value
+    
+        this.setState({
+          itemToAdd: value
+        });
+    }
+
+    sendDoneHandler(event){
+        this.doneHandler(event);
+        
+        this.setState({
+            itemToAdd: ''
+        })
+    }
+
+    render(){
+        return(
+            <>
+                <ul>
+                    {this.lst.map(thing => <ListItem key={thing.id} thing={thing}/>)}
+                </ul>
+                <form onSubmit={this.sendDoneHandler}>
+                    <label>
+                        Add a thing:
+                        <input
+                        name="itemToAdd"
+                        type="textfield"
+                        value={this.state.itemToAdd}
+                        onChange={this.handleInputChange}
+                        />
+                    </label>
+                    <button>submit</button>
+                </form>
+            </>
+        )
+    }
 }
+
+function ListItem(props){
+    return <li>{props.thing.name}</li>
+  }
